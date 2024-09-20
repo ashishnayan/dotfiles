1016a8cea6d14f744caccb21bface30d9a6aa95b
Ashish Nayan <nayanashish@gmail.com>
2024-09-20 18:01:04 +0530

    fixed auto pair

--- nvim/.config/nvim/lua/plugins-config/nvim-autopair.lua
+++ nvim/.config/nvim/lua/plugins-config/nvim-autopair.lua
@@ -1,13 +1,13 @@
-require('nvim-autopairs').setup{
-    fast_wrap = {
-      map = '<leader>e',
-      chars = { '{', '[', '(', '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
-      offset = -1, -- Offset from pattern match
-      end_key = '$',
-      keys = 'qwertyuiopzxcvbnmasdfghjkl',
-      check_comma = true,
-      highlight = 'Search',
-      highlight_grey='Comment'
-    }
+require('nvim-autopairs').setup {
+  fast_wrap = {
+    map = '<C-e>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+    offset = -1, -- Offset from pattern match
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey = 'Comment'
+  }
 }
